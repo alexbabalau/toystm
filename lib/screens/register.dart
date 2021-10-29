ccfd1866a5654d6df569e21cb8c0c17fe9066b2e
Daria Muresan <muresan.daria14@yahoo.com>
2021-10-29 18:15:03 +0300

    Extracted register form into another widget

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toystm/shared/elements/background_image.dart';
-import 'package:toystm/shared/elements/custom_text_field.dart';
 import 'package:toystm/shared/ui_specs.dart';
+import 'package:toystm/widgets/user_register_form.dart';
 
-class Register extends StatefulWidget {
-  const Register({Key? key}) : super(key: key);
-
-  @override
-  _RegisterState createState() => _RegisterState();
-}
-
-class _RegisterState extends State<Register> {
-  var _lastNameController = TextEditingController();
-  var _firstNameController = TextEditingController();
-  var _usernameController = TextEditingController();
-  var _passwordController = TextEditingController();
-  var _passwordVerificationController = TextEditingController();
-  var _emailController = TextEditingController();
-  var _phoneController = TextEditingController();
-
-  bool _isValid = true;
-
+class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,69 +21,7 @@ class _RegisterState extends State<Register> {
                     'ToysTM LOGO',
                     style: TextStyle(fontSize: 30, color: AppColors.DARK),
                   )),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 35),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomTextField('first name', _firstNameController, false),
-                    CustomTextField('last name', _lastNameController, false),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CustomTextField('username', _usernameController, false),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CustomTextField('password', _passwordController, true),
-                    CustomTextField('password verification',
-                        _passwordVerificationController, true),
-                    _isValid
-                        ? SizedBox(
-                            height: 10,
-                          )
-                        : Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '  *Passwords are not the same!',
-                              style: TextStyle(color: AppColors.WINE_RED),
-                            ),
-                          ),
-                    CustomTextField('email', _emailController, false),
-                    CustomTextField(
-                        'phone (optional)', _phoneController, false),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 60),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          AppColors.BRONZE_ORANGE),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                        ),
-                      ),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _passwordController.text ==
-                                _passwordVerificationController.text
-                            ? _isValid = true
-                            : _isValid = false;
-                      });
-                    },
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: AppColors.CREAM, fontSize: 16),
-                    ),
-                  ),
-                ),
-              ),
+              UserRegisterForm(),
             ],
           ),
         ),
