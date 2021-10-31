ab6c728d53358e9baadbc84e604174501f209a1e
Daria Muresan <muresan.daria14@yahoo.com>
2021-10-31 22:33:56 +0200

    Created pendings list

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:toystm/screens/home.dart';
 import 'package:toystm/screens/log_in.dart';
 import 'package:toystm/screens/notifications-center.dart';
+import 'package:toystm/screens/pendings-list.dart';
 import 'package:toystm/screens/register.dart';
 import 'package:toystm/shared/elements/background_image.dart';
 import 'package:toystm/shared/elements/custom_app_bar.dart';
@@ -33,7 +34,8 @@ class _ToysTMAppState extends State<ToysTMApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotificationsCenter(),
+      home: PendingsList(),
+      //NotificationsCenter(),
       //Register(),
       //LogIn(),
       //Home(myProducts),
---
+++ lib/screens/pendings-list.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:toystm/shared/elements/background_image.dart';
+import 'package:toystm/shared/elements/custom_app_bar.dart';
+import 'package:toystm/shared/ui_specs.dart';
+
+class PendingsList extends StatelessWidget {
+  List<Map> pendings = [
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+    {'username': 'dariam', 'date': 'xx/xx/20xx'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.CREAM,
+      appBar: CustomAppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(children: [
+          BackgroundImage("assets/images/img4.png"),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 7),
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    color: AppColors.LIGHT_ORANGE,
+                    child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  pendings[index]['username'],
+                                  style: TextStyle(
+                                    color: AppColors.WINE_RED,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                pendings[index]['date'],
+                                style: TextStyle(
+                                    color: AppColors.WINE_RED, fontSize: 16),
+                              ),
+                            )
+                          ],
+                        )),
+                  );
+                },
+                itemCount: pendings.length),
+          )
+        ]),
+      ),
+    );
+  }
+}
