f4816922472366aac583c37c4fd7f96f0f19f074
Daria Muresan <muresan.daria14@yahoo.com>
2021-10-31 14:15:37 +0200

    Added background image.

--- lib/screens/notifications-center.dart
+++ lib/screens/notifications-center.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toystm/shared/elements/background_image.dart';
 import 'package:toystm/shared/elements/custom_app_bar.dart';
 import 'package:toystm/shared/ui_specs.dart';
 
@@ -13,16 +14,16 @@ class _NotificationsCenterState extends State<NotificationsCenter> {
   List<Map> notifications = [
     {'title': 'Trade request', 'username': 'dariam', 'date': 'xx/xx/20xx'},
     {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
-    // {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
+    {'title': 'Trade response', 'username': 'alexb', 'date': 'xx/xx/20xx'},
   ];
 
   @override
@@ -32,57 +33,64 @@ class _NotificationsCenterState extends State<NotificationsCenter> {
       appBar: CustomAppBar(),
       body: Container(
         height: MediaQuery.of(context).size.height,
-        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        child: ListView.builder(
-            itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40)),
-                color: AppColors.BRONZE_ORANGE,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          notifications[index]['title'],
-                          style: TextStyle(
-                            color: AppColors.CREAM,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 3,
-                      ),
-                      Row(
+        child: Stack(children: [
+          BackgroundImage("assets/images/img3.png"),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 7),
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    color: AppColors.BRONZE_ORANGE,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                      child: Column(
                         children: [
-                          Text(
-                            notifications[index]['username'],
-                            style: TextStyle(
-                              color: AppColors.CREAM,
-                              fontSize: 16,
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              notifications[index]['title'],
+                              style: TextStyle(
+                                color: AppColors.CREAM,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                           SizedBox(
-                            width: 15,
+                            height: 3,
                           ),
-                          Text(
-                            notifications[index]['date'],
-                            style:
-                                TextStyle(color: AppColors.CREAM, fontSize: 16),
+                          Row(
+                            children: [
+                              Text(
+                                notifications[index]['username'],
+                                style: TextStyle(
+                                  color: AppColors.CREAM,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                notifications[index]['date'],
+                                style: TextStyle(
+                                    color: AppColors.CREAM, fontSize: 16),
+                              )
+                            ],
                           )
                         ],
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-            itemCount: notifications.length),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: notifications.length),
+          ),
+        ]),
       ),
     );
   }
