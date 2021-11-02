b50edf55107446345d82bc63a7cc8009b0fe9a41
Daria Muresan <muresan.daria14@yahoo.com>
2021-11-02 22:58:39 +0200

    Added search field and toys grid view.

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:toystm/shared/elements/custom_app_bar.dart';
+import 'package:toystm/shared/elements/custom_text_field.dart';
+import 'package:toystm/shared/ui_specs.dart';
+import 'package:toystm/widgets/toys_grid_view.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -8,8 +12,39 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  var searchController = TextEditingController();
+
+  List<Map> items =
+      List.generate(15, (index) => {'id': index, 'name': 'Product $index'})
+          .toList();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: AppColors.CREAM,
+      appBar: CustomAppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: CustomTextField(
+                'search',
+                searchController,
+                false,
+                backgroundColor: AppColors.WHITE,
+                fieldSuffixIcon: Icon(Icons.search),
+              ),
+            ),
+            ToysGridView(
+              items,
+              height: 90,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/toys_grid_view.dart
+++ lib/widgets/toys_grid_view.dart
@@ -4,15 +4,17 @@ import 'package:toystm/shared/elements/list_item.dart';
 
 class ToysGridView extends StatelessWidget {
   final List<Map> items;
+  double height;
 
-  ToysGridView(this.items);
+  ToysGridView(this.items, {this.height = 0.0});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       height: MediaQuery.of(context).size.height -
-          CustomAppBar().preferredSize.height,
+          CustomAppBar().preferredSize.height -
+          this.height,
       child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 200,
