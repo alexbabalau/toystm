7048569a65ca12c24be6540757552bde5201a6f5
Daria Muresan <muresan.daria14@yahoo.com>
2021-11-03 22:17:46 +0200

    Created Add A Toy screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:toystm/models/pending_transaction.dart';
+import 'package:toystm/screens/add_a_yoy.dart';
 import 'package:toystm/screens/home.dart';
 import 'package:toystm/screens/log_in.dart';
 import 'package:toystm/screens/notifications-center.dart';
@@ -36,7 +37,8 @@ class _ToysTMAppState extends State<ToysTMApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Search(),
+      home: AddAToy(),
+      //Search(),
       //NotificationsCenter(),
       //PendingsList(),
       //Register(),
