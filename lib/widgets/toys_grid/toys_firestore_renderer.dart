3692f1c56d78770b0a3f2ef19a1040b107128e00
alexbabalau <alex.babalau@gmail.com>
2021-11-16 11:36:24 +0200

    Added fetch on scroll capabilities

--- lib/widgets/toys_grid/toys_firestore_renderer.dart
+++ lib/widgets/toys_grid/toys_firestore_renderer.dart
@@ -16,54 +16,25 @@ class ToysFirestoreRenderer extends StatefulWidget {
 class _ToysFirestoreRendererState extends State<ToysFirestoreRenderer> {
   FirestoreService _firestoreService = FirestoreService();
 
-  List<ToyFirestoreModel> toys = [];
-  List<DocumentSnapshot<Map<String, dynamic>>> toyDocuments = [];
-
   ScrollController controller = ScrollController();
 
-  void _fetchFirstPage() async {
-    var firstToyDocuments = await this._firestoreService.getFirstPageOfToys();
-    setState(() {
-      this.toyDocuments = firstToyDocuments;
-      this.toys = this
-        .toyDocuments
-        .map((document) => ToyFirestoreModel.fromDocumentSnapshot(document))
-        .toList();
-    });
-    //print(this.toys);
-  }
+  
 
   @override
   void initState() {
     super.initState();
-    this._fetchFirstPage();
-    controller.addListener(this._scrollListener);
+    //this._fetchFirstPage();
+    //controller.addListener(this._scrollListener);
   }
 
-  void _scrollListener(){
-    print('Here');
-    if (controller.offset >= controller.position.maxScrollExtent &&
-        !controller.position.outOfRange) {
-      print("at the end of list");
-
-      var nextPageOfToyDocuments = await this
-          ._firestoreService
-          .getNextPageOfToys(this.toyDocuments.last);
-      var nextPageOfToys = nextPageOfToyDocuments
-          .map((document) => ToyFirestoreModel.fromDocumentSnapshot(document))
-          .toList();
-      setState(() {
-        this.toyDocuments.addAll(nextPageOfToyDocuments);
-        this.toys.addAll(nextPageOfToys);
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return ToysGridView(
+    /*return ToysGridView(
       toys,
+      controller,
       substractedHeight: this.widget.subtractedHeight,
-    );
+    );*/
+    return Container();
   }
 }
