f89ea2016c604ccd6be5dd7c307199bd78ec8fe4
alexbabalau <alex.babalau@gmail.com>
2021-11-21 00:28:28 +0200

    Started Add Toy Screen

---
+++ lib/screens/add_toy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddToy extends StatefulWidget {
+  const AddToy({ Key? key }) : super(key: key);
+
+  @override
+  _AddToyState createState() => _AddToyState();
+}
+
+class _AddToyState extends State<AddToy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
