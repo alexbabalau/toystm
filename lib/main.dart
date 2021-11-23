106a845ebe9038a565d45a9928e1b5c59a660cba
Daria Muresan <muresan.daria14@yahoo.com>
2021-11-23 23:36:08 +0200

    Added toys grid on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:toystm/models/pending_transaction.dart';
 import 'package:toystm/screens/home.dart';
 import 'package:toystm/screens/log_in.dart';
 import 'package:toystm/screens/my_toy_view.dart';
+import 'package:toystm/screens/my_toys.dart';
 import 'package:toystm/screens/notifications-center.dart';
 import 'package:toystm/screens/pendings-list.dart';
 import 'package:toystm/screens/register.dart';
@@ -33,7 +34,8 @@ class _ToysTMAppState extends State<ToysTMApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyToyView(),
+      home: MyToys(),
+      //MyToyView(),
       //ToyDetails(),
       //UserProfile(),
       //Search(),
