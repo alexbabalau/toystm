1b783d6ef1c9e4ac1acce54dc87bbe7e7ce6cf6f
Daria Muresan <muresan.daria14@yahoo.com>
2021-11-26 16:38:27 +0200

    Added changed background photo.

--- lib/screens/trade_step_3.dart
+++ lib/screens/trade_step_3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toystm/shared/elements/background_image.dart';
 import 'package:toystm/shared/elements/bottom_button.dart';
 import 'package:toystm/shared/elements/custom_app_bar.dart';
 import 'package:toystm/shared/ui_specs.dart';
@@ -10,41 +11,45 @@ class TradeStep3 extends StatelessWidget {
       backgroundColor: AppColors.CREAM,
       appBar: CustomAppBar(),
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
-        child: Column(
+        child: Stack(
           children: [
-            Expanded(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10),
-                    child: Icon(
-                      Icons.check_circle,
-                      color: AppColors.WINE_RED,
-                      size: 140,
-                    ),
+            BackgroundImage("assets/images/img5.png"),
+            Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.check_circle,
+                          color: AppColors.WINE_RED,
+                          size: 140,
+                        ),
+                      ),
+                      Text(
+                        "Trade request sent successfuly!",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontStyle: FontStyle.italic,
+                          color: AppColors.DARK,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Trade request sent successfuly!",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontStyle: FontStyle.italic,
-                      color: AppColors.DARK,
-                    ),
+                ),
+                Expanded(
+                  child: BottomButton(
+                    text: 'pending requests',
+                    backgroundColor: AppColors.WINE_RED,
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: BottomButton(
-                text: 'pending requests',
-                backgroundColor: AppColors.WINE_RED,
-              ),
+                ),
+              ],
             ),
           ],
         ),
