326a947bb7ac7fb75efcab825e5684d194a22c5d
alexbabalau <alex.babalau@gmail.com>
2022-01-09 14:13:00 +0200

    Added Firebase sync for trade step1 sync

--- lib/screens/trade_step_1.dart
+++ lib/screens/trade_step_1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toystm/models/toy.dart';
+import 'package:toystm/models/transaction.dart';
+import 'package:toystm/services/firestore.dart';
 import 'package:toystm/shared/elements/background_image.dart';
 import 'package:toystm/shared/elements/bottom_button.dart';
 import 'package:toystm/shared/elements/custom_app_bar.dart';
@@ -7,8 +9,19 @@ import 'package:toystm/shared/elements/custom_elevated_button.dart';
 import 'package:toystm/shared/elements/toy_short_view.dart';
 import 'package:toystm/shared/ui_specs.dart';
 import 'package:toystm/widgets/toys_grid/toys_firestore_renderer.dart';
+import 'package:toystm/widgets/toys_grid/toys_grid_view.dart';
 
-class TradeStep1 extends StatelessWidget {
+class TradeStep1 extends StatefulWidget {
+  
+  TransactionFirestoreModel transaction;
+
+  TradeStep1({required this.transaction});
+  
+  @override
+  State<TradeStep1> createState() => _TradeStep1State();
+}
+
+class _TradeStep1State extends State<TradeStep1> {
   ToyFirestoreModel toy = ToyFirestoreModel(
     image: 'assets/images/IMG_7805.jpg',
     name: 'Pestera',
@@ -19,42 +32,68 @@ class TradeStep1 extends StatelessWidget {
     dateAdded: DateTime.now(),
   );
 
+  late Future<List<ToyFirestoreModel>> _fetchFuture;
+  
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    this._fetchFuture = FirestoreService().getUserToys(widget.transaction.senderUserId);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.CREAM,
-      appBar: CustomAppBar(),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Stack(
-          children: [
-            BackgroundImage("assets/images/img5.png"),
-            Column(
-              children: [
-                Container(
-                  height: 30,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    
+    return FutureBuilder(
+        future: _fetchFuture,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasError) {
+            throw snapshot.error.toString();
+          }
+          if (snapshot.connectionState == ConnectionState.waiting ||
+              !snapshot.hasData) {
+            //context.loaderOverlay.show();
+            return Container();
+          }
+          
+          return Scaffold(
+            backgroundColor: AppColors.CREAM,
+            appBar: CustomAppBar(),
+            body: Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  BackgroundImage("assets/images/img5.png"),
+                  Column(
                     children: [
-                      Text(
-                        "Select a toy",
-                        style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          color: AppColors.DARK,
+                      Container(
+                        height: 30,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Select a toy",
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                color: AppColors.DARK,
+                              ),
+                            ),
+                          ],
                         ),
+                      ),
+                      ToysGridView(
+                        snapshot.data,
+                        subtractedHeight: 50,
                       ),
                     ],
                   ),
-                ),
-                ToysFirestoreRenderer(
-                  subtractedHeight: 50,
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+          }
+        );
   }
 }
